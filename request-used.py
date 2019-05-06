d00db2911fe473e6d8c53c3b3acf4926504290f0
menh <872204778@qq.com>
2019-05-07 00:03:50 +0800

    re

--- request-used.py
+++ request-used.py
@@ -5,7 +5,7 @@ import requests
 class DownloadHanlder(Thread):
 
     def __init__(self, url):
-        super().__init()
+        super().__init__()
         self.url = url
 
     def run(self):
@@ -18,7 +18,9 @@ class DownloadHanlder(Thread):
         resp = requests.get('http://api.tianapi.com/meinv/?key=APIKey&num=10')
 
         data_model = resp.json()
+        for mm_dict in data_model['newslist']:
+            url = mm_dict['picUrl']
+            DownloadHanlder(url).start()
 
-
-    super().__init__()
-    self.url = url
+if __name__ == '__main__':
+    main()
